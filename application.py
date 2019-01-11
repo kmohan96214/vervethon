11e2826b2e16e38b3bbf7e64f39e6d3592af8d4a
kmohan96214 <kmohan96214@gmail.com>
2019-01-11 15:35:28 +0530

    added 4 models

--- application.py
+++ application.py
@@ -25,9 +25,23 @@ def search():
         plots
         """
         linear = linearRegressionMod(years,csmp)
-        chart = plot(linear[1][0],linear[1][1])
-        charts = [chart]*8
-        return render_template("plot.html",charts = charts,country=country)
-        
+        poly2 = polynomialRegression(years,csmp,2)
+        poly3 = polynomialRegression(years,csmp,3)
+        poly4 = polynomialRegression(years,csmp,4)
+        poly8 = polynomialRegression(years,csmp,8)
+        sarimaxmod = sarimax(years,csmp)
+        hwesmod = hwes(years,csmp)
+
+        charts = []
+        charts.append(plot(years,csmp,"data"))
+        charts.append(plot(linear[1][0],linear[1][1],"linear fit" , years ,csmp))
+        charts.append(plot(poly2[1][0],poly2[1][1], "polynomial fit degree 2" , years ,csmp))
+        charts.append(plot(poly3[1][0],poly3[1][1], "polynomial fit degree 3" , years ,csmp))
+        charts.append(plot(poly4[1][0],poly4[1][1], "polynomial fit degree 4" , years ,csmp))
+        charts.append(plot(poly8[1][0],poly8[1][1],"polynomial fit degree 8" , years ,csmp))
+        charts.append(plot(sarimaxmod[1][0],sarimaxmod[1][1],"sarimax", years ,csmp))
+        charts.append(plot(hwesmod[1][0],hwesmod[1][1],"hwes" ,years ,csmp))
+
+        return render_template("plot.html",charts = charts,country=country)        
 
     
--- helpers.py
+++ helpers.py
@@ -1,8 +1,9 @@
 from flask import redirect, render_template, request, session, url_for
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
-import pprint
 import numpy as np
-
 import matplotlib.pyplot as plt
 import pylab
 
@@ -11,7 +12,7 @@ import plotly.tools as tls
 import plotly.graph_objs as go
 import plotly
 
-plotly.tools.set_credentials_file(username='kmohan962', api_key='tVpcTsGDBQ1SnAb28tZB')
+#plotly.tools.set_credentials_file(username='kmohan962', api_key='tVpcTsGDBQ1SnAb28tZB')
 
 def apology(top="", bottom=""):
     """Renders message as an apology to user."""
@@ -27,17 +28,50 @@ def apology(top="", bottom=""):
         return s
     return render_template("apology.html", top=escape(top), bottom=escape(bottom))
 
-def plot(x,y):
+def plot(x,y,name,*args):
     trace = go.Scatter(x=x,y=y)
-    data = [trace]
-    layout= go.Layout(title="elcsmp", xaxis={'title':'years'}, yaxis={'title':'electricity consumption'})
+    if args:
+        trace1 = go.Scatter(x =args[0],y=args[1])
+        data = [trace,trace1]
+    else:
+        data = [trace]
+    layout= go.Layout(title=name, xaxis={'title':'years'}, yaxis={'title':'electricity consumption'})
     figure =  go.Figure(data=data,layout=layout)
-    return py.iplot(figure)
+    return plotly.offline.plot(figure,output_type="div")
 
 def linearRegressionMod(x,y):
-    print(x,y)
     xvals = np.array(x,dtype=np.dtype('Float64'))
     yvals = np.array(y,dtype=np.dtype('Float64'))
     coefs = pylab.polyfit(xvals,yvals,1)
     estvals = coefs[0]*xvals + coefs[1]
     return [(xvals,yvals),(xvals,estvals)]
+
+def polynomialRegression(x,y,n):
+    xvals = np.array(x,dtype=np.dtype('Float64'))
+    yvals = np.array(y,dtype=np.dtype('Float64'))
+    coefs = pylab.polyfit(xvals,yvals,n)
+    estvals = np.array([0.0 for i in x])
+    for i in range(n+1):
+        estvals = estvals + (coefs[i]*( np.power(xvals,(n-i)) ))
+    return [(xvals,yvals),(xvals,estvals)]
+
+def sarimax(x,y):
+    data = np.array(y,dtype = np.dtype('Float64'))
+    model = SARIMAX(data, order=(1, 1, 1), seasonal_order=(1, 1, 1, 1))
+    model_fit = model.fit(disp=False)
+    yhat = model_fit.predict(0,len(data)-1)
+    return [(x,y),(x,yhat)]
+
+def arima(x,y):
+    data =  np.array(y,dtype = np.dtype('Float64'))
+    model = ARIMA(data, order=(1, 1, 1))
+    model_fit = model.fit(disp=False)
+    yhat = y[0] + model_fit.predict(1, len(data)-1)
+    return [(x,y),(x,yhat)]
+
+def hwes(x,y):
+    data = np.array(y,dtype = np.dtype('Float64'))
+    model = ExponentialSmoothing(data)  
+    model_fit = model.fit()
+    yhat = model_fit.predict(0, len(data)-1)
+    return [(x,y),(x,yhat)]
